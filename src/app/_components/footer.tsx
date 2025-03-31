88df4522c0a1e520dcc47bb44141ac750d6c031a
Sean Hershey <sean.p.hershey@gmail.com>
2025-03-30 21:33:49 -0500

    feat(sybil): mobile ui quick fix

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,7 +4,7 @@ import ChromeExtension from "./ChromeExtension";
 
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-20 flex w-full justify-between gap-2 bg-mf-ash-700 px-2 py-4 text-xs text-mf-milk-700 sm:px-10 sm:py-8 sm:text-base">
+    <div className="fixed bottom-0 left-0 right-0 z-20 flex w-full justify-between gap-2 bg-mf-ash-700 px-2 py-4 text-xs text-mf-milk-700 sm:px-10 sm:py-8 sm:text-base whitespace-nowrap">
       <p className="sm:pl-3">© 2025 Manifold Labs, Inc.</p>
       <div className="flex gap-2">
         <Link href="/privacy">Privacy Policy</Link>
